f638066c41c891b6723e652f19c8a50359b779c0
Shubham Nagar <shubham.nagar@infoaxon.com>
2023-09-15 12:50:09 +0530

    Adding some of Gradle test course activity and sample projects

---
+++ gradle-learning/get-going-with-gradle/hola-gradle/src/main/test/java/com/tomgregory/languageapp/SayHelloTest.java
@@ -0,0 +1,10 @@
+package com.tomgregory.languageapp;
+import org.junit.jupiter.api.Test;
+7
+import java.io.IOException;
+public class SayHelloTest {
+    @Test
+    public void testSayHello() throws IOException {
+        SayHello.main(new String[]{"en"});
+    }
+}
