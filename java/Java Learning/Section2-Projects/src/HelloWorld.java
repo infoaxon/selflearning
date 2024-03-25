d72f61ce14a52fc83ba2c85fbfed830a43b1a840
Shubham Nagar <shubham.nagar@infoaxon.com>
2024-03-25 08:06:22 +0530

    Completed code for Lecture on variables and constants

--- java/Java Learning/Section2-Projects/src/HelloWorld.java
+++ java/Java Learning/Section2-Projects/src/HelloWorld.java
@@ -1,2 +1,7 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Shubham !! is this on ?");
+    }
+
 }
---
+++ java/Java Learning/Section2-Projects/src/VariableFun.java
@@ -0,0 +1,22 @@
+
+public class VariableFun {
+    public static void main(String[] args) {
+        int myAge; // this is the declaration of a variable in camelCase
+        myAge = 19; // this is the initialization of the variable and also called an assignment operator
+
+        final int SOME_NUM = 150; // Use SnakeCase in case of constants, Constants that are symbolic can not be changed
+
+        // Adding code for my Hometown
+
+        String myHomeTown;
+        myHomeTown = "Meerut City";
+
+        String name = "Shubham";
+
+        System.out.print(name + " is.....now... " + myAge + '\n');
+
+        System.out.println(name + "my Home town name is ..." + myHomeTown);
+
+
+    }
+}
