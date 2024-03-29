da3c79f96a792f387737a34d3d1da90d92aca1fe
Shubham Nagar <shubham.nagar@infoaxon.com>
2024-03-29 11:50:07 +0530

    Code for the Arithmetic Lesson

--- java/Java Learning/Section2-Projects/src/ArithmeticFun.java
+++ java/Java Learning/Section2-Projects/src/ArithmeticFun.java
@@ -4,10 +4,43 @@ public class ArithmeticFun {
 
         int a = 56;
         int b = 75;
-
+        // the a and b are the Operands and the + is the operator here
         int result = a + b;
+        // examples of binary operator
+        int difference = a - b;
+        int product = a * b;
+        int quotient = b / a;
+        int remainder = b % a; // this is called modulus
 
-        System.out.println("Result is....." + result);
+        // However in the example below if the Operands are string then the + sign acts as a concatenation instead
+        System.out.println("Result is..... " + result);
+        System.out.println("Product is ..... " + product);
+        System.out.println("diff is ..... " + difference);
+        System.out.println("quotient is ..... " + quotient);
+        System.out.println("remainder is ..... " + remainder);
+        // The below is a UNIRARY operator
+        result +=20; // this takes the result and adds a 20 to it. This is also one of the ways of doing it
+
+        System.out.println("New Result is...." + result);
+        result++; // We increment the result and then print it to learn more about operators. This is called increment operator
+        System.out.println("result++ " + result);
+
+        // Now the challenge code for this lesson is below
+
+        int multipliedProduct;
+        multipliedProduct = product * 2;
+        System.out.println("Multiplied Product is ......" + multipliedProduct);
+
+        // another alternative way is
+
+        product *=2; // Using the compound operator
+
+        System.out.println("New way to Multiplied Product is ......." + product);
+
+
+
+
+
 
     }
 
