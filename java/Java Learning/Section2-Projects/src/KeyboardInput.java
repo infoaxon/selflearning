8d4223afe0232d82c80ef4a66417b522cb4df0a4
Shubham Nagar <shubham.nagar@infoaxon.com>
2024-04-27 10:25:28 +0300

    Adding more capability to Scanner and Keyboard inputs

--- java/Java Learning/Section2-Projects/src/KeyboardInput.java
+++ java/Java Learning/Section2-Projects/src/KeyboardInput.java
@@ -8,6 +8,7 @@ public class KeyboardInput {
     String name;
     int age;
     String city;
+    double rlno;
 
     System.out.println("What is your name ?");
     name = keyboard.nextLine();
@@ -16,12 +17,19 @@ public class KeyboardInput {
     age = keyboard.nextInt();
     keyboard.nextLine();
 
+    System.out.println("Enter a real no.");
+    rlno = keyboard.nextDouble(); // Here we use the Keyboard to input for us a double value
+    keyboard.nextLine();
+    rlno = rlno * 2;
+    System.out.println("Multiplied No. is " + rlno);
+
 
     System.out.println("What city do you live in?");
     city = keyboard.nextLine();
 
     System.out.println("Hello, " + name);
     System.out.println("age is, " + age);
+    System.out.println("Mutiplined No. was " + rlno);
     System.out.println("City is, " + city);
 
     }
